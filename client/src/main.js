9090094b696ad9eca0331a3d5dae272d8f78ac01
infomotin <motin.mmk.jr@gmail.com>
2025-03-16 15:40:42 +0600

    config

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,5 @@
 import './assets/main.css'
+import {echo} from './plugins/echo'
 
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -7,5 +8,6 @@ import router from './router'
 const app = createApp(App)
 
 app.use(router)
+app.use(echo)
 
 app.mount('#app')
