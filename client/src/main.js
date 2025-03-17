9fb27f4ea5729861a935fe8c7ba917ed9baf26b5
infomotin <motin.mmk.jr@gmail.com>
2025-03-17 12:41:34 +0600

    fix cors.php setup

--- client/src/main.js
+++ client/src/main.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-axios.defaults.baseURL ="http://localhost:8000"
+axios.defaults.baseURL ="http://localhost:8000/"
 // axios.defaults.withCredentials = true
 
 const app = createApp(App)
